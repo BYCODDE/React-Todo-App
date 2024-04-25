202311ce673c2cd5b34d775813590c54b819ba0d
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-25 19:07:50 +0400

    new commit!

--- src/components/Main/Todo/Todo.jsx
+++ src/components/Main/Todo/Todo.jsx
@@ -1,8 +1,18 @@
 /* eslint-disable react/prop-types */
+
+import X from "/images/X.svg";
 const Todo = ({ post }) => {
   return (
-    <div className=" border-b-4 border-red-700  bg-background rounded-md shadow-lg tracking-tighter text-customColor4 font-normal leading-normal flex justify-center p-[20px]">
-      <p>{post.content}</p>
+    <div className=" border-b-4 border-red-700  bg-background rounded-md shadow-lg tracking-tighter text-customColor4 font-normal leading-normal flex justify-between p-[20px]">
+      <div className=" shadow-custom  h-[20px] w-[20px] rounded-[50%] bg-white cursor-pointer"></div>
+      <p className="text-customColor3 text-[14px] font-normal leading-normal tracking-tighter">
+        {post.content}
+      </p>
+      <img
+        className="w-[17px] h-[17px]  cursor-pointer"
+        src={X}
+        alt="delete_svg"
+      />
     </div>
   );
 };
